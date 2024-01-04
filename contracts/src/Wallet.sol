d308cdd7ff3e00837479ea0e59638f3f3541606a
Yash Jagtap <jagtapyash2512@gmail.com>
2024-01-04 23:45:17 +0530

    import Base contracts

--- contracts/src/Wallet.sol
+++ contracts/src/Wallet.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.12;
 
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+import {BaseAccount} from "account-abstraction/core/BaseAccount.sol";
+import {UserOperation} from "account-abstraction/interfaces/UserOperation.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract Wallet {
     address public immutable walletFactory;
