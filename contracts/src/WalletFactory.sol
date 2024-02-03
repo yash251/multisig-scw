fca0950e2aa0839bbc13fbaf41483c48209414cc
Yash Jagtap <jagtapyash2512@gmail.com>
2024-02-03 23:11:05 +0530

    ERC1967Proxy imp

--- contracts/src/WalletFactory.sol
+++ contracts/src/WalletFactory.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.12;
 
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
-import {Wallet} from "./Wallet.sol";
+import {Wallet} from "./Wallet.sol"
+
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";;
 
 contract WalletFactory {
     Wallet public immutable walletImplementation;
