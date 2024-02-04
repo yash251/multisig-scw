93f3d3761c7ae9a79fceba34d036885d2d173e29
Yash Jagtap <jagtapyash2512@gmail.com>
2024-02-04 23:05:55 +0530

    Create2 import

--- contracts/src/WalletFactory.sol
+++ contracts/src/WalletFactory.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.12;
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 import {Wallet} from "./Wallet.sol"
 
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";;
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
 contract WalletFactory {
     Wallet public immutable walletImplementation;
