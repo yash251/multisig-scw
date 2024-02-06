d31a69fbb62f4d903286de7d2b1fff976ff14bc7
Yash Jagtap <jagtapyash2512@gmail.com>
2024-02-06 23:30:39 +0530

    getAddress fxn

--- contracts/src/WalletFactory.sol
+++ contracts/src/WalletFactory.sol
@@ -13,4 +13,26 @@ contract WalletFactory {
     constructor(IEntryPoint entryPoint) {
         walletImplementation = new Wallet(entryPoint, address(this));
     }
+
+    function getAddress(
+        address[] memory owners,
+        uint256 salt
+    ) public view returns (address) {
+        // Encode the initialize function in our wallet with the owners array as an argument into a bytes array
+        bytes memory walletInit = abi.encodeCall(Wallet.initialize, owners);
+        // Encode the proxyContract's constructor arguments which include the address walletImplementation and the walletInit
+        bytes memory proxyConstructor = abi.encode(
+            address(walletImplementation),
+            walletInit
+        );
+        // Encode the creation code for ERC1967Proxy along with the encoded proxyConstructor data
+        bytes memory bytecode = abi.encodePacked(
+            type(ERC1967Proxy).creationCode,
+            proxyConstructor
+        );
+        // Compute the keccak256 hash of the bytecode generated
+        bytes32 bytecodeHash = keccak256(bytecode);
+        // Use the hash and the salt to compute the counterfactual address of the proxy
+        return Create2.computeAddress(bytes32(salt), bytecodeHash);
+    }
 }
