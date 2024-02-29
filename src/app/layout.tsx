783b404855b8ba03d7fd34320a4f258bffafd17b
Yash Jagtap <jagtapyash2512@gmail.com>
2024-02-29 11:56:21 +0530

    import components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
+"use client";
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from "@/components/navbar";
+import Providers from "@/components/providers";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
