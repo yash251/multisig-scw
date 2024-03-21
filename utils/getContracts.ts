f8761a5a4153a97db07ace5bbd7c88fceb4f011e
Yash Jagtap <jagtapyash2512@gmail.com>
2024-03-21 23:21:00 +0530

    add getContracts import

---
+++ utils/getContracts.ts
@@ -0,0 +1,9 @@
+import { Contract, providers } from "ethers";
+import { Constants } from "userop";
+import {
+  BUNDLER_RPC_URL,
+  ENTRY_POINT_ABI,
+  WALLET_ABI,
+  WALLET_FACTORY_ABI,
+  WALLET_FACTORY_ADDRESS,
+} from "./constants";
