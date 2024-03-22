09014beb0a946a49bee84f013a894a12c7d9a7df
Yash Jagtap <jagtapyash2512@gmail.com>
2024-03-22 23:18:01 +0530

    add entryPointContract

--- utils/getContracts.ts
+++ utils/getContracts.ts
@@ -7,3 +7,12 @@ import {
   WALLET_FACTORY_ABI,
   WALLET_FACTORY_ADDRESS,
 } from "./constants";
+
+// This needs to be an ethers provider because we will share this with `userop` 
+export const provider = new providers.JsonRpcProvider(BUNDLER_RPC_URL);
+
+export const entryPointContract = new Contract(
+  Constants.ERC4337.EntryPoint,
+  ENTRY_POINT_ABI,
+  provider
+);
